dc6ee20bd0091e28a8f3c74cad7846bdb7bc30d2
chirugampala <40143811+chirugampala@users.noreply.github.com>
2023-04-08 00:00:57 -0700

    Initial Commit

---
+++  App.js
@@ -0,0 +1,8 @@
+const heading = React.createElement("div",{},[
+    React.createElement("h1",{id:"chiru",key:"chiru"},"Testing it"),
+    React.createElement("h3",{id:"nanda",key:"nana"},"Success it"),
+]);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(heading);
